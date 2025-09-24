b1916dab38f48ddedb13bcf846528be26e752c6c
LUCAS SILVA DA ROSA <41279517883@CAMPUS.MACHADO.MCH>
2025-09-24 10:57:23 -0300

    First commit

---
+++ src/Default/Node.java
@@ -0,0 +1,15 @@
+package Default;
+
+import java.util.Comparator;
+
+public class Node<T extends Comparable<T>> {
+	T dado;
+	Node<T> esquerda;
+	Node<T> direita;
+	
+	public Node(T dado) {
+		this.dado = dado;
+		this.esquerda = null;
+		this.direita = null;
+	}
+}
