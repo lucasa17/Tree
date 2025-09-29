b20dac6fb873315a8b5144525b2e35c9f8fb9a79
LUCAS SILVA DA ROSA <41279517883@CAMPUS.MACHADO.MCH>
2025-09-29 10:12:25 -0300

    feat: add BinarySearchtREE

--- src/Default/Main.java
+++ src/Default/Main.java
@@ -3,8 +3,16 @@ package Default;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
+		
+		binarySearchTree.add(20);
+		binarySearchTree.add(10);
+		binarySearchTree.add(50);
+		binarySearchTree.add(35);
+		binarySearchTree.add(15);
+		
+		System.out.println("PreOrder:");
+		binarySearchTree.preOrder();
 	}
 
 }
